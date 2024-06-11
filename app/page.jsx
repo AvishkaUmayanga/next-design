cb4247a8f015c86e3202ddc3fadc40eebb080ad8
AvishkaUmayanga <avishkas97@gmail.com>
2024-06-11 22:30:34 +0530

    update start.module.css

--- app/page.jsx
+++ app/page.jsx
@@ -13,8 +13,8 @@ export default function Home() {
       {/* <HeroSection /> */}
       <FeaturedSection />
       {/* <AnalyticsSection />
-      <Testimonials />
-      <GetStarted /> */}
+      <Testimonials /> */}
+      <GetStarted />
       <Footer />
     </main>
   );
